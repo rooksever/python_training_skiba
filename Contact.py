6ccaf4afda544030b263f6fe9663a94d8f6c8982
rooksever <evg.skiba@gmail.com>
2019-11-28 19:57:36 +0300

    Задание #3

---
+++ Contact.py
@@ -0,0 +1,29 @@
+class Contact:
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company, address, home, mobile_phone, work_phone, fax, email_1, email_2, email_3, homepage, birth_day, birth_mon, birth_year, anniversary_day, anniversary_mon, anniversary_year, address_2, phone_2, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email_1 = email_1
+        self.email_2 = email_2
+        self.email_3 = email_3
+        self.homepage = homepage
+        self.birth_day = birth_day
+        self.birth_mon = birth_mon
+        self.birth_year = birth_year
+        self.anniversary_day = anniversary_day
+        self.anniversary_mon = anniversary_mon
+        self.anniversary_year = anniversary_year
+        self.address_2 = address_2
+        self.phone_2 = phone_2
+        self.notes =notes
+
+
+
