ebc22d5043d642961218febea6e9ad72fcd20edc
rooksever <evg.skiba@gmail.com>
2020-01-17 17:30:13 +0300

    Задание14

--- test/test_contact_info.py
+++ test/test_contact_info.py
@@ -39,3 +39,24 @@ def test_info_on_contact_view_page(app):
     assert contact_from_view_page.fullname == contact_from_edit_page.fullname
 
 
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x), filter(lambda x: x is not None,
+                                                           [contact.home, contact.mobile, contact.work,
+                                                            contact.phone2]))))
+
+
+def test_phones_on_home_page(app):
+    contact_from_home_page = app.contact.get_contacts_list()[0]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+
+
+def test_phones_on_contact_view_page(app):
+    contact_from_view_page = app.contact.get_contact_from_view_page(0)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_view_page.home == contact_from_edit_page.home
+    assert contact_from_view_page.mobile == contact_from_edit_page.mobile
+    assert contact_from_view_page.work == contact_from_edit_page.work
+    assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
+
