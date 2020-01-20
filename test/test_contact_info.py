e6aeb84953b4d3dfc92f3eb6f4a0467809cec684
rooksever <evg.skiba@gmail.com>
2020-01-20 19:20:13 +0300

    Задание14(попытка 5)

--- test/test_contact_info.py
+++ test/test_contact_info.py
@@ -17,6 +17,12 @@ def merge_name_like_on_home_page(contact):
                                                            [contact.first_name, contact.middle_name,
                                                             contact.last_name]))))
 
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x), filter(lambda x: x is not None,
+                                                           [contact.home, contact.mobile, contact.work,
+                                                            contact.phone2]))))
+
 
 def test_info_on_home_page(app):
     contact_from_home_page = app.contact.get_contacts_list()[0]
@@ -27,7 +33,7 @@ def test_info_on_home_page(app):
     assert contact_from_home_page.first_name == contact_from_edit_page.first_name
     assert contact_from_home_page.last_name == contact_from_edit_page.last_name
     assert contact_from_home_page.address == contact_from_edit_page.address
-
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
 def test_info_on_contact_view_page(app):
@@ -37,26 +43,11 @@ def test_info_on_contact_view_page(app):
     assert contact_from_view_page.email2 == contact_from_edit_page.email2
     assert contact_from_view_page.email3 == contact_from_edit_page.email3
     assert contact_from_view_page.fullname == contact_from_edit_page.fullname
-
-
-def merge_phones_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x), filter(lambda x: x is not None,
-                                                           [contact.home, contact.mobile, contact.work,
-                                                            contact.phone2]))))
-
-
-def test_phones_on_home_page(app):
-    contact_from_home_page = app.contact.get_contacts_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-
-
-def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_view_page.home == contact_from_edit_page.home
     assert contact_from_view_page.mobile == contact_from_edit_page.mobile
     assert contact_from_view_page.work == contact_from_edit_page.work
     assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
+
+
+
 
